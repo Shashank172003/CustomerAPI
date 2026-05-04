9d53f3a8b57706c278a68af7f8e8dfcfcc405686
Shashank172003 <shashanknagvanshi00@gmail.com>
2026-05-04 19:12:25 +0530

    initial CustomerRepository

--- CustomerAPI/Repositories/CustomerRepository.cs
+++ CustomerAPI/Repositories/CustomerRepository.cs
@@ -1,6 +1,36 @@
-﻿namespace CustomerAPI.Repositories
+﻿using CustomerAPI.Data;
+using CustomerAPI.Interfaces;
+using CustomerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerAPI.Repositories
 {
-    public class CustomerRepository
+    public class CustomerRepository : ICustomerRepository
     {
+        private readonly AppDbContext _context;
+
+        public CustomerRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Customer> CreateAsync(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+            return customer;
+        }
+
+        public async Task<IEnumerable<Customer>> GetAllAsync()
+        {
+            return await _context.Customers
+                .Where(c => c.IsActive)
+                .ToListAsync();
+        }
+
+        public async Task<Customer?> GetByIdAsync(int id)
+        {
+            return await _context.Customers.FindAsync(id);
+        }
     }
 }
