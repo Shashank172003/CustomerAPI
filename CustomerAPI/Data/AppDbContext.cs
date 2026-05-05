0fb2128864a27216a063d4dbabd4160d29cb9cd8
Shashank172003 <shashanknagvanshi00@gmail.com>
2026-05-05 15:23:20 +0530

    Example Change

--- CustomerAPI/Data/AppDbContext.cs
+++ CustomerAPI/Data/AppDbContext.cs
@@ -5,10 +5,7 @@ namespace CustomerAPI.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){ }
 
        public DbSet<Customer> Customers { get; set; }
 
